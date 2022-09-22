7415c47d7c85dcadb72c00ad73256edbc4ff6b23
Mostafa kamal <110708254+mostafa188@users.noreply.github.com>
2022-09-22 21:42:58 +0600

    Number conversion

---
+++ Octal_to_decimal.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl endl
+//.. .. .. 16 8 4 2 1 
+int bin_to_decimal(int n){
+    int ans = 0;
+    int x =1;
+    while(n>0){
+        int y = n%10;
+        ans += x*y;
+        x*=8;
+        n/=10;
+    }
+    return ans;
+}
+
+int main()
+{
+    cout<<"";
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.precision(2);
+
+    int n;
+    cin>>n;  
+    cout<<bin_to_decimal(n)<<nl;
+    
+    return 0;
+}
